0b95fcc662c81033de798c78b745dc20636e9d11
rlagusalsb <kkhm0929@gmail.com>
2025-09-18 04:46:00 +0900

    feat: OrderService 구현

---
+++ src/main/java/shop/service/OrderService.java
@@ -0,0 +1,48 @@
+package shop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shop.domain.Delivery;
+import shop.domain.Member;
+import shop.domain.Order;
+import shop.domain.OrderItem;
+import shop.domain.items.Item;
+import shop.repository.ItemRepository;
+import shop.repository.MemberRepository;
+import shop.repository.OrderRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(),count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+}
