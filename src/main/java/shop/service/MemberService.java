588aa3b759da00f41a177d374777d9283b0014a1
rlagusalsb <kkhm0929@gmail.com>
2025-09-22 03:59:18 +0900

    fix: MemberService 수정

--- src/main/java/shop/service/MemberService.java
+++ src/main/java/shop/service/MemberService.java
@@ -29,7 +29,7 @@ public class MemberService {
         }
     }
 
-    public List<Member> findMember() {
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
